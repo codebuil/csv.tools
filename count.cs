a366548c4d969395b4b9568d9256d31f2eba68dd
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-01-27 19:20:16 +0000

    Add files via upload

---
+++ count.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvsView
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int nn = 0;
+            float maxsss = 0;
+            int indexss = 0;
+            if (args.Count()>0)
+            {
+                Console.WriteLine(args[0]);
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Clear();
+                try
+                {
+
+                    
+                    string[] lines = System.IO.File.ReadAllLines("" + args[0]);
+
+                    for (n = 0; n < lines.Count(); n++)
+                    {
+                        
+                        string[] fields = lines[n].Split(',');
+                        
+                            
+                            try
+                            {
+                              
+                                
+                                    indexss++;
+                                    
+                                
+                            }
+                            catch (Exception ee)
+                            {
+                                
+                            }
+                        
+
+                    }
+                    Console.Write((indexss).ToString() + "\r\n");
+
+                    Console.Write("\r\n");
+                }
+                catch (Exception ee)
+                {
+                    Console.WriteLine("\n" + Directory.GetCurrentDirectory() + ",file error:");
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n" + Directory.GetCurrentDirectory() + ",file error:");
+            }
+        }
+    }
+}
