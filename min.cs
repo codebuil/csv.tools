5b5d7f884d608b1c2d8e4c0206515603879b6a58
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-01-27 18:21:19 +0000

    Add files via upload

--- min.cs
+++ min.cs
@@ -44,7 +44,7 @@ namespace cvsView
                         }
                         else
                         {
-                            indexss = 0;
+                            
                             try
                             {
                                float maxssss = float.Parse(fields[nnnn].Trim());
