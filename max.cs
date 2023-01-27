53bfc6ee88d47b6a5eb5e6f4b4f894ee782bf8fb
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-01-27 17:41:02 +0000

    .\max.exe bi.csv 2

---
+++ max.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvsView
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int nn = 0;
+            float maxsss = 0;
+            int indexss = 0;
+            if (args.Count()>1)
+            {
+                Console.WriteLine(args[0]);
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Clear();
+                try
+                {
+
+                    int nnnn = int.Parse(args[1].Trim());
+                    string[] lines = System.IO.File.ReadAllLines("" + args[0]);
+
+                    for (n = 0; n < lines.Count(); n++)
+                    {
+                        string[] fields = lines[n].Split(',');
+                        if (n == 0)
+                        {
+                            indexss = 0;
+                            try
+                            {
+                                maxsss = float.Parse(fields[nnnn].Trim());
+                            }
+                            catch(Exception ee)
+                            {
+                                maxsss = 0;
+                            }
+                        }
+                        else
+                        {
+                            indexss = 0;
+                            try
+                            {
+                               float maxssss = float.Parse(fields[nnnn].Trim());
+                                if (maxssss > maxsss)
+                                {
+                                    indexss = n;
+                                    maxsss = maxssss;
+                                }
+                            }
+                            catch (Exception ee)
+                            {
+                                
+                            }
+                        }
+
+                    }
+                    Console.Write(lines[indexss] + "\r\n");
+
+                    Console.Write("\r\n");
+                }
+                catch (Exception ee)
+                {
+                    Console.WriteLine("\n" + Directory.GetCurrentDirectory() + ",file error:");
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n" + Directory.GetCurrentDirectory() + ",file error:");
+            }
+        }
+    }
+}
