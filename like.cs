2c879c18cc1b6286026645bd3bdf2872cbf1b1db
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-01-27 19:36:27 +0000

    .\like.exe bi.csv dis

---
+++ like.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvsView
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int nn = 0;
+            float maxsss = 0;
+            int indexss = 0;
+            if (args.Count()>1)
+            {
+                Console.WriteLine(args[0]);
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Clear();
+                try
+                {
+
+                    
+                    string[] lines = System.IO.File.ReadAllLines("" + args[0]);
+
+                    for (n = 0; n < lines.Count(); n++)
+                    {
+                        try
+                        {
+                            int i = lines[n].IndexOf(args[1]);
+                            if(i>-1)Console.WriteLine(lines[n]);
+                        }
+                        catch (Exception ee)
+                        { 
+                        }
+                        
+                    }  
+                }
+                catch (Exception ee)
+                {
+                    Console.WriteLine("\n" + Directory.GetCurrentDirectory() + ",file error:");
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n" + Directory.GetCurrentDirectory() + ",file error:");
+            }
+        }
+    }
+}
